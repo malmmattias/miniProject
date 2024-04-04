a6ea008165b25442faf2ef51427ca42acdac3c8c
AdamLeijman <adam.leijman@hotmail.com>
2024-04-04 09:19:04 +0200

    Skapat klasser vi behöver

---
+++ src/Model/Condition.java
@@ -0,0 +1,8 @@
+package Model;
+
+public enum Condition {
+    NEW,
+    VERY_GOOD,
+    GOOD,
+    NOT_WORKING_PROPERLY
+}
--- src/Model/Product.java
+++ src/Model/Product.java
@@ -1,4 +1,9 @@
 package Model;
 
 public class Product {
+    private int price;
+    private int productionYear;
+    private String color;
+    private Condition condition;
+    private Status status;
 }
