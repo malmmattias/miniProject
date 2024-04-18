81be3267d96080048455a35f3e42eeade9317a13
martin24olsson <128680764+martin24olsson@users.noreply.github.com>
2024-04-18 11:26:05 +0200

    aaa

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -2,7 +2,12 @@ package Controller;
 
 public class Main {
     public static void main(String[] args) {
+        Server server = new Server();
+    }
+
+    public static void startClient(String[] args) {
         Client client = new Client();
     }
+
 
 }
