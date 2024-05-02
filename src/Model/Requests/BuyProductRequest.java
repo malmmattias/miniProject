2060de9fe8e92006757bc93313fd004934b83f5a
AdamLeijman <adam.leijman@hotmail.com>
2024-05-02 09:35:53 +0200

    ...

--- src/Model/Requests/BuyProductRequest.java
+++ src/Model/Requests/BuyProductRequest.java
@@ -1,6 +1,18 @@
 package Model.Requests;
 
+import Model.Product;
 import Model.Request;
 
+import java.util.ArrayList;
+
 public class BuyProductRequest extends Request {
+    private ArrayList<Product> products;
+
+    public BuyProductRequest(ArrayList<Product> cart) {
+        products = cart;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
 }
