559bb5b37694b5349626127cf17ceb7b53bb5dc0
martin24olsson <128680764+martin24olsson@users.noreply.github.com>
2024-05-11 14:30:23 +0200

    CodeMR report

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+J
+src/Controller/Client.java,6/2/624f126aab36a1345c5c8e6a7262c6746caffea7
---
+++ codemr/miniProject/html/html_report_miniproject/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,14 @@
+var matrix = [[0,0,0,0],[2,0,0,2],[0,0,0,0],[8,0,0,0]]
+var packages = [{
+"name": " Model", "color": " #3182bd"
+}
+,{
+"name": " Controller", "color": " #6baed6"
+}
+,{
+"name": " View", "color": " #9ecae1"
+}
+,{
+"name": " Model.Requests", "color": " #c6dbef"
+}
+];
